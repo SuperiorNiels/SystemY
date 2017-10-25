548532b40805f5d8fd309510d0f3d2a336ef3cf5
wartek69 <alextcher@hotmail.com>
2017-10-25 12:32:58 +0200

    made a loop in client

--- NameServer/Client.java
+++ NameServer/Client.java
@@ -24,60 +24,68 @@ public class Client {
     }
 
     public void communication(){
-        String name,ip;
-        scanner = new Scanner(System.in);
-        System.out.println("press 1 to add a node to the server");
-        System.out.println("press 2 to remove a node");
-        System.out.println("press 3 to add a filename to the server");
-        System.out.println("press 4 to remove a filename from the serer");
-        System.out.println("press 5 to get the owner of the file");
-        System.out.println("press 6 to export an xml file");
-        int answer = scanner.nextInt();
-        try {
-            switch(answer){
-                case 1:
+        boolean inLoop = true;
+        while(inLoop) {
+            String name, ip;
+            scanner = new Scanner(System.in);
+            System.out.println("press 1 to add a node to the server");
+            System.out.println("press 2 to remove a node");
+            System.out.println("press 3 to add a filename to the server");
+            System.out.println("press 4 to remove a filename from the serer");
+            System.out.println("press 5 to get the owner of the file");
+            System.out.println("press 6 to export an xml file");
+            System.out.println("Press 7 to exit");
+            int answer = scanner.nextInt();
+            try {
+                switch (answer) {
+                    case 1:
 
-                    System.out.print("Node Name :");
-                    name = scanner.next();
-                    System.out.print("Ip :");
-                    ip = scanner.next();
-                    try{
-                        NamingServer.addNode(ip , name);
-                    }catch(AlreadyExistsException e){
-                        System.out.println("Hash, name or ip already exists");
-                    }
+                        System.out.print("Node Name :");
+                        name = scanner.next();
+                        System.out.print("Ip: ");
+                        ip = scanner.next();
+                        try {
+                            NamingServer.addNode(ip, name);
+                        } catch (AlreadyExistsException e) {
+                            System.out.println("Hash, name or ip already exists");
+                        }
 
-                    break;
-                case 2:
-                    System.out.print("Node Name :");
-                    name = scanner.next();
-                    NamingServer.removeNode(name);
-                    break;
-                case 3:
-                    System.out.println("FileName: ");
-                    name = scanner.next();
-                    NamingServer.addFile(name);
-                    break;
-                case 4:
-                    System.out.println("FileName: ");
-                    name = scanner.next();
-                    NamingServer.removeFile(name);
-                    break;
-                case 5:
-                    System.out.println("FileName: ");
-                    name = scanner.next();
-                    NamingServer.getOwner(name);
-                    break;
-                case 6:
-                    try {
-                        NamingServer.createXML("test");
-                    }catch(Exception e){
-                        System.out.println("error exporting xml "+e.getMessage());
-                    }
+                        break;
+                    case 2:
+                        System.out.print("Node Name: ");
+                        name = scanner.next();
+                        NamingServer.removeNode(name);
+                        break;
+                    case 3:
+                        System.out.println("FileName: ");
+                        name = scanner.next();
+                        NamingServer.addFile(name);
+                        break;
+                    case 4:
+                        System.out.println("FileName: ");
+                        name = scanner.next();
+                        NamingServer.removeFile(name);
+                        break;
+                    case 5:
+                        System.out.println("FileName: ");
+                        name = scanner.next();
+                        NamingServer.getOwner(name);
+                        break;
+                    case 6:
+                        try {
+                            NamingServer.createXML("./data/output.xml");
+                        } catch (Exception e) {
+                            System.out.println("error exporting xml " + e.getMessage());
+                        }
+                        break;
+                    case 7:
+                        System.out.println("bye bye!");
+                        inLoop= false;
 
+                }
+            } catch (RemoteException e) {
+                System.err.println("error with RMI server" + e.getMessage());
             }
-        }catch(RemoteException e){
-            System.err.println("error with RMI server"+e.getMessage());
         }
 
     }
