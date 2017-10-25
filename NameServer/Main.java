79f7b895a268c3b096d46e776001cb10d52ba7fe
niels <balemans.niels@gmail.com>
2017-10-25 12:16:49 +0200

    changed main

--- NameServer/Main.java
+++ NameServer/Main.java
@@ -8,6 +8,8 @@ public class Main {
         } else if(args[0].toLowerCase().equals("client")) {
             Client client = new Client();
             client.communication();
+        } else if(args[0].isEmpty()) {
+            System.out.println("No parameter used, please use server/client");
         } else {
             System.out.println("Please use parameter server/client.");
         }
