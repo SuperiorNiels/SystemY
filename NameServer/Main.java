e2fbec3cce164b45de3c5fd3c6d0cc19da47cf22
niels <balemans.niels@gmail.com>
2017-10-25 12:18:24 +0200

    changed main

--- NameServer/Main.java
+++ NameServer/Main.java
@@ -3,15 +3,18 @@ package NameServer;
 public class Main {
 
     public static void main(String[] args) {
-        if(args[0].toLowerCase().equals("server")) {
-            NamingServer server = new NamingServer();
-        } else if(args[0].toLowerCase().equals("client")) {
-            Client client = new Client();
-            client.communication();
-        } else if(args[0].isEmpty()) {
+        try {
+            if (args[0].toLowerCase().equals("server")) {
+                NamingServer server = new NamingServer();
+            } else if (args[0].toLowerCase().equals("client")) {
+                Client client = new Client();
+                client.communication();
+            } else {
+                System.out.println("Please use parameter server/client.");
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("No parameter used, please use server/client");
-        } else {
-            System.out.println("Please use parameter server/client.");
         }
     }
 }
