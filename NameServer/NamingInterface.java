3d87f1a40de07099f58ef2308a85caa32a72894d
Dieter97 <dieter.balemans@hotmail.com>
2017-10-25 12:15:13 +0200

    implemented RMI server in NamingServer

--- NameServer/NamingInterface.java
+++ NameServer/NamingInterface.java
@@ -4,10 +4,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface NamingInterface extends Remote {
-    public String createXML() throws RemoteException,Exception;
+    public void createXML(String path) throws RemoteException,Exception;
     public void addNode(String ip, String name) throws RemoteException, AlreadyExistsException;
     public void removeNode(String name) throws RemoteException;
-    public void addFile(String name) throws RemoteException;
+    public int addFile(String name) throws RemoteException;
     public void removeFile(String fileName) throws RemoteException;
-    public void getOwner(String fileName) throws RemoteException;
+    public String getOwner(String fileName) throws RemoteException;
 }
