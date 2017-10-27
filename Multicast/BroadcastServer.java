31de1e7ec04b86709d8b541c286aab8b86a10cf8
Dieter97 <dieter.balemans@hotmail.com>
2017-10-27 11:21:12 +0200

    Added Mulitcast Test

---
+++ Multicast/BroadcastServer.java
@@ -0,0 +1,45 @@
+package Multicast;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class BroadcastServer {
+
+    public static void main(String[] args) throws IOException {
+        Scanner snc = new Scanner(System.in);
+        System.out.println("Type a message: ");
+        String in = snc.nextLine();
+        new BroadcastServerThread(in).start();
+    }
+
+}
+
+class BroadcastServerThread extends Thread{
+    private DatagramSocket socket;
+    private String message;
+    public BroadcastServerThread(String message){
+        this.message = message;
+        try {
+            socket = new DatagramSocket(4446);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run(){
+        try {
+            byte[] buf = new byte[256];
+            buf = message.getBytes();
+            InetAddress groupAddress = InetAddress.getByName("10.0.1.0");
+            DatagramPacket packet;
+            packet = new DatagramPacket(buf, buf.length, groupAddress, 4446);
+            socket.send(packet);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            socket.close();
+        }
+    }
+}
