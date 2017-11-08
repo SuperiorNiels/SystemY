c389a7e80b7fa4c1b59976d58ccc091c613e7819
niels <balemans.niels@gmail.com>
2017-11-08 10:57:15 +0100

    added neighbour class

--- Node/NodeInterface.java
+++ Node/NodeInterface.java
@@ -4,8 +4,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface NodeInterface extends Remote {
-    public Node getPrevious() throws RemoteException;
-    public Node getNext() throws RemoteException;
-    public void setNext(Node nextNode) throws RemoteException;
-    public void setPrevious(Node previousNode) throws RemoteException;
+    public Neighbour getPrevious() throws RemoteException;
+    public Neighbour getNext() throws RemoteException;
+    public void setNext(Neighbour nextNode) throws RemoteException;
+    public void setPrevious(Neighbour previousNode) throws RemoteException;
 }
