b8c21d2385263b8e7b30718c98e6318d2421859d
wartek69 <alextcher@hotmail.com>
2017-11-13 20:34:57 +0100

    added connectionTCP

---
+++ Network/ConnectionTCP.java
@@ -0,0 +1,72 @@
+package Network;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ConnectionTCP extends Thread {
+    private Socket clientSocket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String filePath;
+
+    /**
+     *
+     * @param aClientSocket
+     */
+    public ConnectionTCP(Socket aClientSocket) {
+        try {
+            filePath = new File(".").getCanonicalPath();
+            clientSocket = aClientSocket;
+            //We wrap everything in bufferedstream to fasten up the transaction
+            //The reason for using datastream is because we can write/read primitive types straigth from/to the input/output
+            in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            out = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+            //starts the thread
+            this.start();
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     */
+    public void run() {
+        try {
+            String data = in.readUTF();
+            //array of bytes that holds the file bytes
+            byte[] file = readFile(data);
+            System.out.println("sending file :"+data);
+            out.flush();        //flushes the buffer
+            out.write(file,0,file.length);
+        } catch (IOException e) {
+            System.out.println("readline:" + e.getMessage());
+        }finally{
+            try{
+                clientSocket.close();
+            }catch(IOException e){
+                System.out.println("problem closing the socket: "+e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * reads a file into a byte array and returns that array
+     * @param fileName
+     * @return
+     */
+    private byte[] readFile(String fileName){
+        File myFile = new File(filePath+"\\"+fileName);
+        byte myByteArray[] = new byte[(int) myFile.length()];
+        try {
+            BufferedInputStream reader = new BufferedInputStream(new FileInputStream(myFile));
+            reader.read(myByteArray,0,myByteArray.length);
+            reader.close();
+        }catch(FileNotFoundException e){
+            System.out.println("The file has not been found: "+e.getMessage());
+        }catch(IOException e){
+            System.out.println("problem with reading the file: "+e.getMessage());
+        }
+        return myByteArray;
+    }
+}
