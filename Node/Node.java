3e16731c67746819bb95947982670ba8130733d5
jamie wubben <wubben.jamie@outlook.com>
2017-11-18 17:19:28 +0100

    added updateFilesNewNode

--- Node/Node.java
+++ Node/Node.java
@@ -193,6 +193,7 @@ public class Node implements NodeInterface, Observer {
 
     /**
      * This method updates the nodes next and previous neighbours
+     * and starts the method to update the files when this nodes becomes the previous.
      * @param new_name, String name of the new node (received via multicast)
      * @param new_ip, String ip address of the new node
      */
@@ -219,6 +220,8 @@ public class Node implements NodeInterface, Observer {
                 }
                 //Update next with new node
                 next = new Neighbour(new_name, new_ip);
+                //after updating the neighbours update the files.
+                updateFilesNewNode(next);
             } else if(calculateHash(previous.getName()) > new_hash && new_hash < my_hash) {
                 //I'm the next node
                 //The new node becomes your previous
@@ -420,5 +423,34 @@ public class Node implements NodeInterface, Observer {
             System.err.println("Problem opening port "+destPort);
         }
 
+    }
+
+    /**
+     * node checks all files he owns (via hash)
+     * compares hash with new node (next)
+     * if hash(file) is closer to hash next
+     * send file to next, update nameserver about owner
+     * @param next
+     */
+    public void updateFilesNewNode(Neighbour next){
+
+        int hashNext = calculateHash(next.getName());
+        int destPort = 8000;
+        String pathFilesReplication = "\\filesReplication";
+        //get file of all files this node owns
+        File folder = new File(pathFilesReplication);
+        File[] listOfFiles = folder.listFiles();
+        //for every file
+            for (int i = 0; i < listOfFiles.length; i++) {
+                //calculate hash
+                int hashFile = calculateHash(listOfFiles[i].getName());
+                //if hash file >= hash next
+                if(hashFile >= hashNext ){
+                    //sent via tcp to next
+                    sendFile(next.getIp(),destPort,listOfFiles[i].getPath(),listOfFiles[i].getName());
+                    //notify nameserver that next is now owner of file
+                    //this node is now download location of file
+                }
+            }
     }
 }
