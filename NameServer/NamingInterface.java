7be79dd76a6b763565a190452a7e48dcbfed81b3
wartek69 <alextcher@hotmail.com>
2018-01-11 14:25:38 +0100

    revert

--- NameServer/NamingInterface.java
+++ NameServer/NamingInterface.java
@@ -6,11 +6,11 @@ import java.rmi.RemoteException;
 import Node.Neighbour;
 
 public interface NamingInterface extends Remote {
-    void createXML(String path) throws RemoteException,Exception;
-    void addNode(String ip, String name) throws RemoteException, AlreadyExistsException;
-    void removeNode(String name) throws RemoteException, NullPointerException;
-    Neighbour getOwner(String fileName) throws RemoteException;
-    Neighbour findNextNode(String nameFailedNode) throws RemoteException;
-    Neighbour findPreviousNode(String nameFailedNode) throws RemoteException;
-    int getNumberOfNodes() throws RemoteException;
+    public void createXML(String path) throws RemoteException,Exception;
+    public void addNode(String ip, String name) throws RemoteException, AlreadyExistsException;
+    public void removeNode(String name) throws RemoteException, NullPointerException;
+    public Neighbour getOwner(String fileName) throws RemoteException;
+    public Neighbour findNextNode(String nameFailedNode) throws RemoteException;
+    public Neighbour findPreviousNode(String nameFailedNode) throws RemoteException;
+    public int getNumberOfNodes() throws RemoteException;
 }
